ad206cdc9187634f3fb1d8dffbcb4b53ca8c9a1b
cyperx84 <cyperx84@gmail.com>
2025-05-11 23:17:42 +1000

    🔥

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -67,8 +67,8 @@ vim.keymap.set("n", "<leader>nd", "<cmd>Neogit diff kind=auto<CR>", { desc = "Ne
 vim.keymap.set("n", "<leader>nl", "<cmd>Neogit log kind=auto<CR>", { desc = "Neogit Log" })
 vim.keymap.set("n", "<leader>ns", "<cmd>Neogit stash kind=auto<CR>", { desc = "Neogit Stash" })
 vim.keymap.set("n", "<leader>nm", "<cmd>Neogit merge kind=auto<CR>", { desc = "Neogit Merge" })
-vim.keymap.set("n", "<leader>np", "<cmd>Neogit pull<CR>", { desc = "Neogit pull" })
-vim.keymap.set("n", "<leader>nP", "<cmd>Neogit push<CR>", { desc = "Neogit push" })
+vim.keymap.set("n", "<leader>nP", "<cmd>Neogit pull<CR>", { desc = "Neogit pull" })
+vim.keymap.set("n", "<leader>np", "<cmd>Neogit push<CR>", { desc = "Neogit push" })
 
 -- Dismiss Noice Message
 vim.keymap.set("n", "<leader>dd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
